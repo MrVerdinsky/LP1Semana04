b3be564cdece262c451715b213b0de83abf8cc8a
MrVerdinsky <goncalo.vila.verde@hotmail.com>
2022-03-20 15:31:45 +0000

    Add Fibonacci Exercise

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fibonacci(8));
+        }
+
+        private static int Fibonacci(int n)
+        {
+            int x;
+            if (n <= 2)
+            {
+                x = 1;
+            }
+            else
+            {
+                x = Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+            return x;
+        }
+    }
+}
