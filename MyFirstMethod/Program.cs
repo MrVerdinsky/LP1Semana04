37eacd54e425bdf9df4928bd5c9ea6efa4be6ce7
MrVerdinsky <goncalo.vila.verde@hotmail.com>
2022-03-20 14:50:17 +0000

    Add XML Documentation to MyFirstMethod

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -4,19 +4,32 @@ namespace MyFirstMethod
 {
     class Program
     {
+        /// <summary>
+        /// Main method that runs the program
+        /// </summary>
+        /// <param name="args">Arguments passed when running
+        /// the program</param>
         static void Main(string[] args)
         {
             CountToN(5);
             CountToN(20);
-            CountTo10(10);
+            CountTo10();
         }
 
-        private static void CountTo10(int n)
+        /// <summary>
+        /// Method used to count to 10 using the CountToN
+        /// method
+        /// </summary>
+        private static void CountTo10()
         {
-            CountToN(n);
+            CountToN(10);
         }
             
-
+        /// <summary>
+        /// Method used to count to N number using the given
+        /// value
+        /// </summary>
+        /// <param name="n">Value to be count to</param>
         private static void CountToN(int n)
         {
             for (int i = 0; i <= n; i++)
